43c91337faad3e43f44fa542ecc1c99f1cad49d0
Servontius Turner <servontius.turner@me.com>
2024-07-15 23:11:30 -0500

    fixed Integer.parse error

--- src/main/java/org/servontiusturner/Main.java
+++ src/main/java/org/servontiusturner/Main.java
@@ -5,24 +5,33 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		// Print out the Random Number for testing
-		// System.out.println(chosenNumber);
-		NumberGuesser guessingGame = new NumberGuesser();
+
+		int numberOfGuesses;
+		int cpuNumber;
 		String playAgain;
+
+		NumberGuesser guessingGame = new NumberGuesser();
 
 		System.out.print("What is your name: ");
 		guessingGame.setPlayerName(scanner.nextLine());
 		guessingGame.greetUser();
 
 		do {
-			guessingGame.setPlayerGuess(Integer.parseInt(scanner.nextLine()));
+			System.out.println("\n \nI am thinking of a number between 1 and 100. Can you guess it? \n \nPlease choose a number between 1 and 100: ");
+			int playerGuess = Integer.parseInt(scanner.next());
+			guessingGame.setPlayerGuess(playerGuess);
 
-			guessingGame.guessPlayerNumber(scanner);
+			guessingGame.guessPlayerNumber();
+			numberOfGuesses = guessingGame.getGuessCount();
+			cpuNumber = guessingGame.getCpuNumber();
+
+			System.out.println("Congratulations " + guessingGame.getPlayerName() + "! You guessed my number! It was " + cpuNumber);
+			System.out.println("It took " + numberOfGuesses + " guesses!");
 
 			System.out.println("Try Again (y/n)?: ");
-			playAgain = scanner.nextLine();
-
-			
+			playAgain = scanner.nextLine();			
 		} while (playAgain.equalsIgnoreCase("y"));
+		
+		scanner.close();
 	}
 }
--- src/main/java/org/servontiusturner/NumberGuesser.java
+++ src/main/java/org/servontiusturner/NumberGuesser.java
@@ -4,19 +4,37 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuesser {
-	private static String playerName;
+	private String playerName;
 	private int guess;
-	
+	private int guessCount;
+	private int cpuNumber;
+
+	public int getCpuNumber() {
+		return cpuNumber;
+	}
+
+	public void setCpuNumber(int cpuNumber) {
+		this.cpuNumber = cpuNumber;
+	}
+
+	public int getGuessCount() {
+		return guessCount;
+	}
+
+	public void setGuessCount(int guessCount) {
+		this.guessCount = guessCount;
+	}
+
 	public NumberGuesser() {}
 
 	public void setPlayerName(String playerName) {
-		NumberGuesser.playerName = playerName;
+		this.playerName = playerName;
 	}
-	
+
 	public String getPlayerName() {
 		return playerName;
 	}
-	
+
 	public void setPlayerGuess(int playerGuess) {
 		this.guess = playerGuess;
 	}
@@ -31,30 +49,33 @@ public class NumberGuesser {
 	}
 
 	public void greetUser() {
-         System.out.print("Hi " + playerName + "!" + "\n \nI am thinking of a number between 1 and 100. Can you guess it? \n \nPlease choose a number between 1 and 100: " );
+		System.out.print("Hi " + playerName + "!");
 	}
-	
-	public void guessPlayerNumber(Scanner scanner) {
-        Random randomNumber = new Random();
 
-        //Choose a random number between 1 and 101 not including 101
-        int cpuNumber = randomNumber.nextInt(1, 101);
-        int guessCount = 0;
-        int currentUserGuess = guess;
+	public void guessPlayerNumber() {
+		Random randomNumber = new Random();
+		Scanner scanner = new Scanner(System.in);
+		
+		//Choose a random number between 1 and 101 not including 101
+		int cpuNumber = randomNumber.nextInt(1, 101);
+		int guessCount = 0;
+		int currentUserGuess = this.guess;
 
-        while (currentUserGuess != cpuNumber) {
-            if (currentUserGuess > cpuNumber) {
-                System.out.print("Too high. Guess again: ");
-            } else {
-                System.out.print("Too low. Guess again: ");
-            }
-            currentUserGuess = Integer.parseInt(scanner.nextLine());
-            guessCount ++;
-        }
-        
-        guessCount++;
-        
-        System.out.println("Congratulations " + playerName + "! You guessed my number! It was " + cpuNumber);
-        System.out.println("It took " + guessCount + " guesses!");
-    }
+		while (currentUserGuess != cpuNumber) {
+			if (currentUserGuess > cpuNumber) {
+				System.out.print(currentUserGuess + " Is too high. Guess again: ");
+			} else {
+				System.out.print(currentUserGuess + " Is too low. Guess again: ");
+			}
+			currentUserGuess = Integer.parseInt(scanner.nextLine());
+			guessCount ++;
+		}
+
+		guessCount++;
+
+		setPlayerGuess(currentUserGuess);
+		setGuessCount(guessCount);
+		setCpuNumber(cpuNumber);
+		scanner.close();
+	}
 }
