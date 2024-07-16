14a06c5752847ae438d7edfa65b87f66890010e8
Servontius Turner <servontius.turner@me.com>
2024-07-15 21:21:02 -0500

    Added game loop for repeat playing

--- src/main/java/org/servontiusturner/Main.java
+++ src/main/java/org/servontiusturner/Main.java
@@ -3,19 +3,26 @@ package org.servontiusturner;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // Print out the Random Number for testing
-        // System.out.println(chosenNumber);
-        NumberGuesser guessingGame = new NumberGuesser();
-        
-        
-        System.out.print("What is your name: ");
-        guessingGame.setPlayerName(scanner.nextLine());
-        guessingGame.greetUser();
-        
-        guessingGame.setPlayerGuess(Integer.parseInt(scanner.nextLine()));
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		// Print out the Random Number for testing
+		// System.out.println(chosenNumber);
+		NumberGuesser guessingGame = new NumberGuesser();
+		String playAgain;
 
-        guessingGame.guessPlayerNumber(scanner);
-    }
+		System.out.print("What is your name: ");
+		guessingGame.setPlayerName(scanner.nextLine());
+		guessingGame.greetUser();
+
+		do {
+			guessingGame.setPlayerGuess(Integer.parseInt(scanner.nextLine()));
+
+			guessingGame.guessPlayerNumber(scanner);
+
+			System.out.println("Try Again (y/n)?: ");
+			playAgain = scanner.nextLine();
+
+			
+		} while (playAgain.equalsIgnoreCase("y"));
+	}
 }
